4c41f39c5ba8dcc9bc3f8c71f7c2ee74205c6583
Vasanth <142874622+Vasanth527@users.noreply.github.com>
2023-10-07 21:06:32 +0530

    Add files via upload

---
+++ day 8 - 100 days of code/day 8 start/main.py
@@ -0,0 +1,27 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet(vasanth):
+  print("vasanhth" + " "+  vasanth)
+  print("sanjay")
+  print("aswin")
+greet("slayer")
+
+def addition(value1,value2):
+  print(value1 - value2)
+addition(value2 = 10,value1 = 5)
+
+a = int(input("enter the number"))
+b = int(input("enter the number"))
+def addition(c,d):
+  c = a+b
+  print(c)
+addition(a,b)
+
+
+def marriage (he,she):
+  wed = he+" "+"weds" + " "+she
+  print(wed)
+marriage(he="vas",she="future")
